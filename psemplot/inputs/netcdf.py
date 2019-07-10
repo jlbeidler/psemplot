aba024b67d43ff197154e4c249f200253d039a35
James Beidler <james.beidler@gmail.com>
2019-07-10 15:05:04 -0400

    input interface updates

--- psemplot/inputs/netcdf.py
+++ psemplot/inputs/netcdf.py
@@ -6,16 +6,17 @@ class PsempData:
     """
     def __init__(self, infile_name):
         self.src = ncf.Dataset(infile_name)
+        self.filename = infile_name
         self.get_attr()
 
     def __str__(self):
-        return self.src.filename
+        return self.filename
 
     def var(self, var_name):
-        if var_name in list(self.variables.keys()):
+        if var_name in list(self.src.variables.keys()):
             arr = self.src.variables[var_name]
         else:
-            raise ValueError('The variable %s does not exist in the file %s.' %(var_name, self.src.filename))
+            raise ValueError('The variable %s does not exist in the file %s.' %(var_name, self.filename))
         return arr[:]
 
     def close(self):
@@ -30,9 +31,9 @@ class PsempData:
                 'ycell': 'YCELL', 'sdate': 'SDATE', 'tstep': 'TSTEP', 'gdtyp': 'GDTYP', 'pgam': 'P_GAM'}
         for out_attr, in_attr in attr_dict.items():
             try:
-                val = self.src.in_attr
+                val = getattr(self.src, in_attr)
             except AttributeError:
-                raise AttributeError('Attribute %s not found in %s' %(in_attr, self.src.filename))
+                raise AttributeError('Attribute %s not found in %s' %(in_attr, self.filename))
             else:
                 setattr(self, out_attr, val)
 
